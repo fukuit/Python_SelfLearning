f54d128c965e48ead72185fc4d5c8e252fe5d2c4
FUKUI Toshifumi <fukuit@mac.com>
2017-01-07 18:44:53 +0900

    adapt to pep8.

--- chainer_lesson/mnist_train.py
+++ chainer_lesson/mnist_train.py
@@ -1,6 +1,14 @@
 import numpy as np
 import chainer
-from chainer import cuda, Function, gradient_check, report, training, utils, Variable
+from chainer import (
+    cuda,
+    Function,
+    gradient_check,
+    report,
+    training,
+    utils,
+    Variable,
+)
 from chainer import datasets, iterators, optimizers, serializers
 from chainer import Link, Chain, ChainList
 import chainer.functions as F
@@ -12,23 +20,25 @@ train, test = datasets.get_mnist()
 
 # define train/test dataset
 train_iter = iterators.SerialIterator(train, batch_size=100, shuffle=True)
-test_iter = iterators.SerialIterator(test, batch_size=100, repeat=False, shuffle=False)
+test_iter = iterators.SerialIterator(
+    test, batch_size=100, repeat=False, shuffle=False)
+
 
 # define three layer network
 class MLP(Chain):
-	def __init__(self, n_units, n_out):
-		super(MLP, self).__init__(
-			l1 = L.Linear(None, n_units),
-			l2 = L.Linear(None, n_units),
-			l3 = L.Linear(None, n_out)
-		)
-		
-	def __call__(self, x):
-		h1 = F.relu(self.l1(x))
-		h2 = F.relu(self.l2(h1))
-		y = self.l3(h2)
-		return y
-	
+    def __init__(self, n_units, n_out):
+        super(MLP, self).__init__(
+            l1=L.Linear(None, n_units),
+            l2=L.Linear(None, n_units),
+            l3=L.Linear(None, n_out)
+        )
+
+    def __call__(self, x):
+        h1 = F.relu(self.l1(x))
+        h2 = F.relu(self.l2(h1))
+        y = self.l3(h2)
+        return y
+
 # using pre-defined classifier
 model = L.Classifier(MLP(100, 10))
 optimizer = optimizers.SGD()
@@ -42,6 +52,7 @@ trainer = training.Trainer(updater, (20, 'epoch'), out='result')
 # invoke training loop
 trainer.extend(extensions.Evaluator(test_iter, model))
 trainer.extend(extensions.LogReport())
-trainer.extend(extensions.PrintReport(['epoch', 'main/accuracy', 'validation/main/accuracy']))
+trainer.extend(extensions.PrintReport(
+    ['epoch', 'main/accuracy', 'validation/main/accuracy']))
 trainer.extend(extensions.ProgressBar())
 trainer.run()

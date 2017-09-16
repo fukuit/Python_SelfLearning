93f5562a75047e1a2c189d49b3c5d21eb7e306d3
FUKUI Toshifumi <fukuit@mac.com>
2017-09-17 00:23:07 +0900

    add subplot() of binarized image.

--- binarize_image2.py
+++ binarize_image2.py
@@ -13,12 +13,16 @@ if os.path.exists(lenna):
     orig = cv2.imread(lenna, 0)
     img = cv2.medianBlur(orig, 5)
     ret, th = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    img_low = img < ret
+    img[img_low] = 0
 
-    plt.subplot(1, 2, 1), plt.imshow(orig, 'gray')
+    plt.subplot(1, 3, 1), plt.imshow(orig, 'gray')
     plt.title('Original')
     plt.xticks([]), plt.yticks([])
-    plt.subplot(1, 2, 2), plt.imshow(img, 'gray')
+    plt.subplot(1, 3, 2), plt.imshow(img, 'gray')
     plt.title('Threshold')
     plt.xticks([]), plt.yticks([])
-
+    plt.subplot(1, 3, 3), plt.imshow(th, 'gray')
+    plt.title('Binarize')
+    plt.xticks([]), plt.yticks([])
     plt.show()

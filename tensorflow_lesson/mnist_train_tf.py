753306503753651e8294f4a544bef74cad78ec0b
FUKUI Toshifumi <fukuit@mac.com>
2017-10-19 21:20:20 +0900

    cross_entropyの計算方法の変更

--- tensorflow_lesson/mnist_train_tf.py
+++ tensorflow_lesson/mnist_train_tf.py
@@ -19,7 +19,9 @@ y = tf.nn.softmax(tf.matmul(x, W) + b)
 # 正解データ
 y_ = tf.placeholder(tf.float32, [None, 10])
 # cross_entropy: 最適化パラメーター
-cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+#cross_entropy = tf.reduce_mean(
+#    -tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y, y_))
 # 最急降下法による最適化パラメーターの最小化
 train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 
@@ -37,7 +39,7 @@ for i in range(1000):
     sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
 
 # 正解率の表示
-correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+correct_prediction = tf.equal(
+    tf.argmax(y, 1), tf.argmax(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
-
